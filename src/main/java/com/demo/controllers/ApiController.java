bac221d521c258664a0a012f43de9934086f81a6
Naveed Khan <m.naveed.u.khan.n@gmail.com>
2021-10-26 01:47:08 +0500

    demo 1

---
+++ src/main/java/com/demo/controllers/ApiController.java
@@ -0,0 +1,19 @@
+package com.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> hi(@RequestParam(required = false) String name) {
+        if (name != null && !name.isEmpty()) {
+            return ResponseEntity.ok(String.format("Hi, %s Welcome to localhost.", name));
+        }
+        return ResponseEntity.ok(String.format("Hi, Welcome to localhost.", name));
+    }
+
+}
